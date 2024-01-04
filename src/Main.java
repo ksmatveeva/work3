102bfdca92d4c1001f59e6c55f8f50672c89aea9
Ksenia <ksyu.matveeva.98@internet.ru>
2024-01-04 21:33:15 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,45 @@
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+
+public class Main {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
+        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
+        Collection<Person> persons = new ArrayList<>();
+
+        for (int i = 0; i < 10_000_000; i++) {
+            persons.add(new Person(
+                    names.get(new Random().nextInt(names.size())),
+                    families.get(new Random().nextInt(families.size())),
+                    new Random().nextInt(100),
+                    Sex.values()[new Random().nextInt(Sex.values().length)],
+                    Education.values()[new Random().nextInt(Education.values().length)])
+            );
+        }
+
+        // Задание 1
+        long underageCount = persons.stream()
+                .filter(person -> person.getAge() < 18)
+                .count();
+        System.out.println("Количество несовершеннолетних: " + underageCount);
+
+        // Задание 2
+        List<String> conscriptSurnames = persons.stream()
+                .filter(person -> person.getSex() == Sex.MAN && person.getAge() >= 18 && person.getAge() <= 27)
+                .map(Person::getFamily)
+                .collect(Collectors.toList());
+        System.out.println("Список фамилий призывников: " + conscriptSurnames);
+
+        // Задание 3
+        List<Person> potentialWorkers = persons.stream()
+                .filter(person -> person.getEducation() == Education.HIGHER &&
+                        ((person.getSex() == Sex.WOMAN && person.getAge() >= 18 && person.getAge() <= 60) ||
+                                (person.getSex() == Sex.MAN && person.getAge() <= 65)))
+                .sorted(Comparator.comparing(Person::getFamily))
+                .collect(Collectors.toList());
+        System.out.println("Отсортированный по фамилии список потенциально работоспособных людей: " + potentialWorkers);
+    }
+}
+
