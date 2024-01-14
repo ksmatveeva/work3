ab081b693e16a8a6e1d010103b219a90c1ea2f69
Ksenia <ksyu.matveeva.98@internet.ru>
2024-01-14 18:46:48 +0300

    Новые изменения

--- src/Main.java
+++ src/Main.java
@@ -1,46 +1,16 @@
 import java.util.*;
-import java.util.stream.Collectors;
-
 
 public class Main {
     public static void main(String[] args) {
-        List<String> names = Arrays.asList("Jack", "Connor", "Harry", "George", "Samuel", "John");
-        List<String> families = Arrays.asList("Evans", "Young", "Harris", "Wilson", "Davies", "Adamson", "Brown");
-        Collection<Person> persons = new ArrayList<>();
+        List<Integer> numbers = Arrays.asList(1, 2, 5, 16, -1, -2, 0, 32, 3, 5, 8, 23, 4);
+        List<Integer> result = new ArrayList<>();
 
-        for (int i = 0; i < 10_000_000; i++) {
-            persons.add(new Person(
-                    names.get(new Random().nextInt(names.size())),
-                    families.get(new Random().nextInt(families.size())),
-                    new Random().nextInt(100),
-                    Sex.values()[new Random().nextInt(Sex.values().length)],
-                    Education.values()[new Random().nextInt(Education.values().length)])
-            );
+        for (Integer x : numbers) {
+            if (x > 0 && x % 2 == 0) {
+                result.add(x);
+            }
         }
 
-        long underageCount = persons.stream()
-                .filter(person -> person.getAge() < 18)
-                .count();
-        System.out.println("Количество несовершеннолетних: " + underageCount);
-
-// Задание 2
-        List<String> conscriptSurnames = persons.stream()
-                .filter(person -> person.getSex() == Sex.MAN && person.getAge() >= 18 && person.getAge() <= 27)
-                .map(Person::getFamily)
-                .collect(Collectors.toList());
-
-        System.out.println("Список фамилий призывников: " + conscriptSurnames);
-
-// Задание 3
-        List<String> potentialWorkers = persons.stream()
-                .filter(person -> (person.getSex() == Sex.WOMAN && person.getAge() >= 18 && person.getAge() <= 60) ||
-                        (person.getSex() == Sex.MAN && person.getAge() >= 18 && person.getAge() <= 65))
-                .filter(person -> person.getEducation() == Education.HIGHER)
-                .sorted(Comparator.comparing(Person::getFamily))
-                .map(Person::getFamily)
-                .collect(Collectors.toList());
-
-        System.out.println("Отсортированный по фамилии список потенциально работоспособных людей: " + potentialWorkers);
+        System.out.println(result);
     }
 }
-
